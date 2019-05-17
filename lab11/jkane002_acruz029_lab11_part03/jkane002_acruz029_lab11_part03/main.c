b2886b4442f00070146a7e053334bd9ddcc88a3c
Jonathan Kaneshiro <jkaneshiro97@gmail.com>
2019-05-17 11:57:02 -0700

    lab 11 part 1 & 3

---
+++ lab11/jkane002_acruz029_lab11_part03/jkane002_acruz029_lab11_part03/main.c
@@ -0,0 +1,262 @@
+/*	Partner 1 Name & E-mail: Jonathan Kaneshiro jkane002@ucr.edu
+*	Partner 2 Name & E-mail: Andrew Cruz acruz029@ucr.edu
+*	Lab Section: #24
+*	Assignment: Lab 11 Exercise 3
+*	Exercise Description:
+*	Combine the functionality of the keypad and LCD so when keypad is pressed and released, 
+*	the character of the button pressed is displayed on the LCD, and stays displayed until 
+*	a different button press occurs (May be accomplished with two tasks: LCD interface & 
+*	modified test harness).
+*/
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include "io.c"
+
+volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C programmer should clear to 0.
+
+// Internal variables for mapping AVR's ISR to our cleaner TimerISR model.
+unsigned long _avr_timer_M = 1; // Start count from here, down to 0. Default 1 ms.
+unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1ms ticks
+
+void TimerOn() {
+	// AVR timer/counter controller register TCCR1
+	TCCR1B = 0x0B;// bit3 = 0: CTC mode (clear timer on compare)
+	// bit2bit1bit0=011: pre-scaler /64
+	// 00001011: 0x0B
+	// SO, 8 MHz clock or 8,000,000 /64 = 125,000 ticks/s
+	// Thus, TCNT1 register will count at 125,000 ticks/s
+
+	// AVR output compare register OCR1A.
+	OCR1A = 125;	// Timer interrupt will be generated when TCNT1==OCR1A
+	// We want a 1 ms tick. 0.001 s * 125,000 ticks/s = 125
+	// So when TCNT1 register equals 125,
+	// 1 ms has passed. Thus, we compare to 125.
+	// AVR timer interrupt mask register
+	TIMSK1 = 0x02; // bit1: OCIE1A -- enables compare match interrupt
+
+	//Initialize avr counter
+	TCNT1=0;
+
+	_avr_timer_cntcurr = _avr_timer_M;
+	// TimerISR will be called every _avr_timer_cntcurr milliseconds
+
+	//Enable global interrupts
+	SREG |= 0x80; // 0x80: 1000000
+}
+
+void TimerOff() {
+	TCCR1B = 0x00; // bit3bit1bit0=000: timer off
+}
+
+void TimerISR() {
+	TimerFlag = 1;
+}
+
+// In our approach, the C programmer does not touch this ISR, but rather TimerISR()
+ISR(TIMER1_COMPA_vect) {
+	// CPU automatically calls when TCNT1 == OCR1 (every 1 ms per TimerOn settings)
+	_avr_timer_cntcurr--; // Count down to 0 rather than up to TOP
+	if (_avr_timer_cntcurr == 0) { // results in a more efficient compare
+		TimerISR(); // Call the ISR that the user uses
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+
+// Set TimerISR() to tick every M ms
+void TimerSet(unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
+unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b) {
+	return (b ? x | (0x01 << k) : x & ~(0x01 << k));
+}
+
+unsigned char GetBit(unsigned char x, unsigned char k) {
+	return ((x & (0x01 << k)) != 0);
+}
+
+//--------Find GCD function --------------------------------------------------
+unsigned long int findGCD(unsigned long int a, unsigned long int b)
+{
+	unsigned long int c;
+	while(1){
+		c = a%b;
+		if(c==0){return b;}
+		a = b;
+b = c;
+	}
+	return 0;
+}
+//--------End find GCD function ----------------------------------------------
+
+unsigned char GetKeypadKey() {
+
+	PORTC = 0xEF; // Enable col 4 with 0, disable others with 1?s
+	asm("nop"); // add a delay to allow PORTC to stabilize before checking
+	if (GetBit(PINC,0)==0) { return('1'); }
+	if (GetBit(PINC,1)==0) { return('4'); }
+	if (GetBit(PINC,2)==0) { return('7'); }
+	if (GetBit(PINC,3)==0) { return('*'); }
+
+	// Check keys in col 2
+	PORTC = 0xDF; // Enable col 5 with 0, disable others with 1?s
+	asm("nop"); // add a delay to allow PORTC to stabilize before checking
+	if (GetBit(PINC,0)==0) { return('2'); }
+	if (GetBit(PINC,1)==0) { return('5'); }
+	if (GetBit(PINC,2)==0) { return('8'); }
+	if (GetBit(PINC,3)==0) { return('0'); }
+
+	// Check keys in col 3
+	PORTC = 0xBF; // Enable col 6 with 0, disable others with 1?s
+	asm("nop"); // add a delay to allow PORTC to stabilize before checking
+	if (GetBit(PINC,0)==0) { return('3'); }
+	if (GetBit(PINC,1)==0) { return('6'); }
+	if (GetBit(PINC,2)==0) { return('9'); }
+	if (GetBit(PINC,3)==0) { return('#'); }
+
+	// Check keys in col 4
+	PORTC = 0x7F;
+	asm("nop");
+	if (GetBit(PINC,0)==0) { return('A'); }
+	if (GetBit(PINC,1)==0) { return('B'); }
+	if (GetBit(PINC,2)==0) { return('C'); }
+	if (GetBit(PINC,3)==0) { return('D'); }
+
+	return('\0'); // default value
+
+}
+
+
+//--------Task scheduler data structure---------------------------------------
+// Struct for Tasks represent a running process in our simple real-time operating system.
+typedef struct _task {
+	/*Tasks should have members that include: state, period,
+		a measurement of elapsed time, and a function pointer.*/
+	signed char state; //Task's current state
+	unsigned long int period; //Task period
+	unsigned long int elapsedTime; //Time elapsed since last task tick
+	int (*TickFct)(int); //Task tick function
+} task;
+
+//--------End Task scheduler data structure-----------------------------------
+
+//--------Shared Variables----------------------------------------------------
+unsigned char pressed_char, curr_char;
+//--------End Shared Variables------------------------------------------------
+
+//--------User defined FSMs---------------------------------------------------
+//Enumeration of states.
+enum SM1_States { KeyPress };
+enum SM2_States { LCD_W };
+
+// Monitors button connected to PA0. 
+// When button is pressed, shared variable "pause" is toggled.
+int SMTick1(int state) {
+
+	switch (state){
+		case KeyPress:
+			state = KeyPress;
+		break;
+		default:
+			state = KeyPress;
+	}
+
+	switch (state){
+			case KeyPress:
+				pressed_char = GetKeypadKey();
+			break;
+			default:
+			break;
+		}
+	return state;
+}
+
+int SMTick2(int state){
+	switch(state){
+		case LCD_W:
+			state = LCD_W;
+		default:
+			state = LCD_W;
+			break;
+	}
+	switch(state){
+		case LCD_W:
+			if(curr_char != pressed_char && pressed_char != '\0'){
+				LCD_Cursor(1);
+				LCD_WriteData(pressed_char);
+				curr_char = pressed_char;
+			}
+		default:
+		break;
+	}
+
+	return state;
+}
+
+// --------END User defined FSMs-----------------------------------------------
+
+// Implement scheduler code from PES.
+int main()
+{
+// Set Data Direction Registers
+DDRD = 0xFF; PORTD = 0x00;
+DDRA = 0xFF; PORTD = 0x00;
+DDRB = 0xFF; PORTB = 0x00;
+DDRC = 0xF0; PORTC = 0x0F;
+// . . . etc
+LCD_init();
+
+// Period for the tasks
+unsigned long int SMTick1_calc = 50;
+
+//Calculating GCD
+unsigned long int tmpGCD = 1;
+
+//Greatest common divisor for all tasks or smallest time unit for tasks.
+unsigned long int GCD = tmpGCD;
+
+//Recalculate GCD periods for scheduler
+unsigned long int SMTick1_period = SMTick1_calc/GCD;
+
+//Declare an array of tasks 
+static task task1,task2;
+task *tasks[] = { &task1, &task2};
+const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
+
+// Task 1
+task1.state = -1;//Task initial state.
+task1.period = SMTick1_period;//Task Period.
+task1.elapsedTime = SMTick1_period;//Task current elapsed time.
+task1.TickFct = &SMTick1;//Function pointer for the tick.
+
+task2.state = -1;
+task2.period = SMTick1_period;
+task2.elapsedTime = SMTick1_period;
+task2.TickFct = &SMTick2;
+
+// Set the timer and turn it on
+TimerSet(GCD);
+TimerOn();
+
+unsigned short i; // Scheduler for-loop iterator
+while(1) {
+	// Scheduler code
+	for ( i = 0; i < numTasks; i++ ) {
+		// Task is ready to tick
+		if ( tasks[i]->elapsedTime == tasks[i]->period ) {
+			// Setting next state for task
+			tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+			// Reset the elapsed time for next tick.
+			tasks[i]->elapsedTime = 0;
+		}
+		tasks[i]->elapsedTime += 1;
+	}
+	while(!TimerFlag);
+	TimerFlag = 0;
+}
+
+// Error: Program should not exit!
+return 0;
+}
